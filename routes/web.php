e22476b5f5e7f6fab0cbba12582f3e1561d2262c
CJ <vooncj@gmail.com>
2019-03-17 21:17:32 +0800

    crud done

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/admin/tenant/create', 'TenantController@create')->name('tenant.create');
+Route::post('admin/tenant', 'TenantController@store')->name('tenant.store');
+Route::get('admin/tenant', 'TenantController@index')->name('tenant.index');
+Route::get('admin/tenant/{tenant}','TenantController@show')->name('tenant.show');
+Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::resource('/admin/tenant', 'TenantController');
+Route::get('admin/tenant/{tenant}/upload', 'tenantController@upload')->name('tenant.upload');
+Route::post('admin/tenant/{tenant}/save-upload', 'tenantController@saveUpload')->name('tenant.saveUpload');
+
+
